dffd02a86a366139272ff9af2628d5f2ca3b6503
megamattz <217563273+megamattz@users.noreply.github.com>
2026-02-05 19:47:38 +1100

    added binding properties for the filter and sort popup

--- Noter/ViewModels/FilterAndSortViewModel.cs
+++ Noter/ViewModels/FilterAndSortViewModel.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using CommunityToolkit.Maui.Views;
+using Noter.CoreBusiness;
 
 namespace Noter.ViewModels
 {
@@ -16,6 +13,91 @@ namespace Noter.ViewModels
 		public ICommand ClosePopup { get; }
 
 		private Popup? _popup;
+
+		private SortingColumn _sortingColumn;
+		private SortDirection _sortDirection;
+
+		private bool _showGeneralNotesCategory;
+		private bool _showStarredNotesCategory;
+		private bool _showListNotesCategory;
+		private bool _showFunNotesCategory;
+
+		public SortingColumn SortingColumn
+		{
+			get
+			{
+				return _sortingColumn;
+			}
+			set
+			{
+				_sortingColumn = value;
+				OnPropertyChanged(nameof(SortingColumn));
+			}
+		}
+
+		public SortDirection SortDirection
+		{
+			get
+			{
+				return _sortDirection;
+			}
+			set
+			{
+				_sortDirection = value;
+				OnPropertyChanged(nameof(SortDirection));
+			}
+		}
+
+		public bool ShowGeneralNotesCategory
+		{
+			get
+			{
+				return _showGeneralNotesCategory;
+			}
+			set
+			{
+				_showGeneralNotesCategory = value;
+				OnPropertyChanged(nameof(ShowGeneralNotesCategory));
+			}
+		}
+
+		public bool ShowStarredNotesCategory
+		{
+			get
+			{
+				return _showStarredNotesCategory;
+			}
+			set
+			{
+				_showStarredNotesCategory = value;
+			}
+		}
+
+		public bool ShowListNotesCategory
+		{
+			get
+			{
+				return _showListNotesCategory;
+			}
+			set
+			{
+				_showListNotesCategory = value;
+				OnPropertyChanged(nameof(ShowListNotesCategory));
+			}
+		}
+
+		public bool ShowFunNotesCategory
+		{
+			get
+			{
+				return _showFunNotesCategory;
+			}
+			set
+			{
+				_showFunNotesCategory = value;
+				OnPropertyChanged(nameof(ShowFunNotesCategory));
+			}
+		}
 
 		public FilterAndSortViewModel()
 		{
@@ -33,6 +115,23 @@ namespace Noter.ViewModels
 			{
 				await _popup.CloseAsync();
 			}
+		}
+
+		protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		private NoteCategories[] GetSelectedCategories()
+		{
+			List<NoteCategories> noteCategories = new List<NoteCategories>();
+
+			if (_showListNotesCategory) {noteCategories.Add(NoteCategories.General); }
+			if (_showStarredNotesCategory) { noteCategories.Add(NoteCategories.Starred); }
+			if (_showListNotesCategory) { noteCategories.Add(NoteCategories.Tick); }
+			if (_showFunNotesCategory) { noteCategories.Add(NoteCategories.Game); }
+
+			return noteCategories.ToArray();
 		}
 	}
 }
